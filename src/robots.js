78719b1db2466cc05fc9bbda2af25609e4aad68c
DESKTOP-DDJ0DSO\binhpfiev <binhpfiev@gmail.com>
2020-04-07 00:46:09 +0900

    Initital

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Test',
+        username: 'Hek',
+        email: 'no132@mail.com'
+    },
+    {
+        id: 2,
+        name: 'Maer',
+        username: 'Can I',
+        email: 'icbm@mail.com'
+    },
+    {
+        id: 3,
+        name: 'Kall',
+        username: 'Hello I',
+        email: 'eztcp@mail.com'
+    },
+]
